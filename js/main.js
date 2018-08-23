427b932a15de291ba58662280034d58b8295c609
yen-zshen <dianna0904@gmail.com>
2018-08-23 19:59:01 +0800

    0758
    0758

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 
-$(document).ready(function (e) {
-    $(".hvr-img div").hover(function () {
-//        $(".hvr-img div").css('background-color','rgba(0,0,0,0.5)')
-//        $(".hvr-img div").css('display', 'flex');
-//        $(".hvr-img div").css('align-items', 'center');
-//        $(".hvr-img div").css('justify-content', 'center');
-//        $(".hvr-img div").css('flex-direction', 'column');
-//          $(".hvr-img p").css('display', 'block');
-//        $(".hvr-img h4").css('display', 'block');
-    }, function () {
-//        $(".hvr-img div").css('background-color','rgba(0,0,0,0)')
-//        $(".hvr-img div").css('display', 'none');
-//          $(".hvr-img p").css('display', 'none');
-//        $(".hvr-img h4").css('display', 'none');
-    });
+$(document).ready(function () {
+   
 });
