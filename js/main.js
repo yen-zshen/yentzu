644a85bbd1f3503fa7551e791eed3c49a7187e0f
yen-zshen <dianna0904@gmail.com>
2018-08-29 17:31:33 +0800

    0531
    0531

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 
 $(document).ready(function () {
-   
+   $('.hvr-img div').click(function(){
+       $(this).css({
+         "background-color" : "rgba(0, 0, 0, 0.5)", 
+         " align-items": "center",
+         "color":"rgb(255, 255, 255)"
+         });
+       
+       $(this).parent().siblings().find('div').css({
+         "background-color" : "rgba(0, 0, 0, 0.0)", 
+         " align-items": "center",
+         "color":"rgba(0, 0, 0, 0.0)" 
+       })
+       
+//       $('.hvr-img img').fadeOut();
+//       alert('123');
+   });
+    
+    $('.hvr-img div').hover(function(){
+        $(this).css({
+         "background-color" : "rgba(0, 0, 0, 0.5)", 
+         " align-items": "center",
+         "color":"rgb(255, 255, 255)"
+         });
+        $(this).parent().siblings().find('div').css({
+         "background-color" : "rgba(0, 0, 0, 0.0)", 
+         " align-items": "center",
+         "color":"rgba(0, 0, 0, 0.0)" 
+       })
+    });
+    
 });
+
